deba3564334cadf902bd30d456282668a01a8e8e
NirvanS <nirvan.sharma95@gmail.com>
2017-11-18 21:05:26 -0400

    Intro Firebase OAuth
    Added a new component that takes care of login. Current functionality uses just facebook and google api to login.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyARJaQ6IU3ScMvwNA5PIo3gVJl_U45M7Vo",
+  authDomain: "uwichem.firebaseapp.com",
+  databaseURL: "https://uwichem.firebaseio.com",
+  projectId: "uwichem",
+  storageBucket: "uwichem.appspot.com",
+  messagingSenderId: "297331885020"
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, HostBinding  } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import * as firebase from 'firebase/app';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  error: any;
+  user: Observable<firebase.User>;
+  //items: FirebaseListObservable<any[]>;
+
+  constructor(public ngFireAuth: AngularFireAuth) {   //add router variable after 
+
+    this.user = ngFireAuth.authState;
+    
+  }
+
+  ngOnInit() {
+  }
+
+  loginWithGoogle() {
+    this.ngFireAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
+
+  loginWithFacebook(){
+    this.ngFireAuth.auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider());
+  }
+
+  loginWithEmail(){
+    this.ngFireAuth.auth.signInWithRedirect(new firebase.auth.EmailAuthProvider());
+  }
+
+  logout() {
+    this.ngFireAuth.auth.signOut();
+  }
+
+}
