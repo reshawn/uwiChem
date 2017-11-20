3a2a297e4910749bd50d36d6c5731b5d142be426
Reshawn Ramjattan <reshawnramjattan@gmail.com>
2017-11-19 21:31:01 -0400

    Added layout of main screen
    - Tabs using angular material 5
    - Swipe to switch tabs using hammerJS for gestures
    - Turned off view encapsulation to allow for child components to be styled in css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,17 @@ import { LoginComponent } from './login/login.component';
 
 import {AngularFireModule} from 'angularfire2';
 import {AngularFireAuthModule} from 'angularfire2/auth';
+import { MainComponent } from './main/main.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+import {
+  MatTabsModule,
+  MatGridListModule,
+} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 
 export const firebaseConfig = {
   apiKey: "AIzaSyARJaQ6IU3ScMvwNA5PIo3gVJl_U45M7Vo",
@@ -17,17 +28,35 @@ export const firebaseConfig = {
   messagingSenderId: "297331885020"
 }
 
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'main', component: MainComponent },
+
+];
+
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    MatTabsModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatGridListModule,
+    RouterModule.forRoot(
+      appRoutes
+    ),
   ],
-  providers: [],
+  providers: [{ 
+    provide: HAMMER_GESTURE_CONFIG, 
+    useClass: HammerGestureConfig 
+}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, ViewChild, ViewChildren } from '@angular/core';
+import {ViewEncapsulation} from '@angular/core';
+import {MatTabGroup, MatTab} from '@angular/material';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  encapsulation: ViewEncapsulation.None, //needed in order to style child components of mat with css
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+  @ViewChild(MatTabGroup) group;
+  @ViewChildren(MatTab) tabs;
+  tab_num = 2;
+  selected = 0;
+  // constant for swipe action: left or right
+  SWIPE_ACTION = { LEFT: 'swipeleft', RIGHT: 'swiperight' };
+
+  selectChange(): void{ //update selected state if user changes by clicking header
+    this.selected = this.group.selectedIndex;
+    console.log("Selected INDEX: " + this.selected);
+
+  }
+
+  swipe(eType){ //change selected tab if user swipes the content of a tab
+    console.log(eType);
+    if(eType === this.SWIPE_ACTION.RIGHT && this.selected > 0){
+      console.log("movin left")
+      this.selected--;
+    }
+    else if(eType === this.SWIPE_ACTION.LEFT && this.selected < this.tab_num){
+      console.log("movin right")
+      this.selected++;
+    }
+    console.log(this.selected)
+  }
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
