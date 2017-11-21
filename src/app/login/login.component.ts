a294d9581186d5d05730f0810feb063f1849db27
NirvanS <nirvan.sharma95@gmail.com>
2017-11-21 18:37:56 -0400

    Save User Email from OAuth
    Added functionality to save user emails as well as their unique IDs into our firebase after they sign in through facebook or google.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, HostBinding  } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
 import { Router } from '@angular/router';
 import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
 import * as firebase from 'firebase/app';
-import {LoginService} from './login.service';
+import { LoginService } from './login.service';
+
 
 @Component({
   selector: 'app-login',
@@ -13,21 +14,21 @@ import {LoginService} from './login.service';
 })
 export class LoginComponent implements OnInit {
 
-  authstate : any ;
+  authstate: any;
   error: any;
 
-  constructor(private router: Router, public loginservice : LoginService) {   //add router variable after 
-    
+  constructor(private router: Router, public loginservice: LoginService) {   //add router variable after 
+
   }
 
   ngOnInit() {
   }
 
-  signInGoogle(){
+  signInGoogle() {
     this.loginservice.loginWithGoogle();
   }
 
-  signInFacebook(){
+  signInFacebook() {
     this.loginservice.loginWithFacebook();
   }
 
