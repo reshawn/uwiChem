ea81093d14017bc7510a427612c0fa67a692f736
Reshawn Ramjattan <reshawnramjattan@gmail.com>
2017-12-01 00:15:29 -0400

    Styled authentication page
    Looks pretty :)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'UwiChem';
+  title = 'UWIchem';
   constructor(private notificationService: NotificationService){
     this.notificationService.getPermission();
   }
