8b835aa80f553334c1693b2b5b8eda4004889823
NirvanS <nirvan.sharma95@gmail.com>
2017-12-03 20:02:14 -0400

    De-console logged everything else
    removed some commented code that was not needed anymore as well :bowtie:

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -37,94 +37,46 @@ export class CoursesComponent implements OnInit {
 
   ngOnInit() {
     this.coursesObservable = this.getCourses('/Chemistry/courses');
-    
-    console.log(this.coursesObservable);
-    this.userList = this.getCourses('/Chemistry/users');
-     this.user_ID = this.lgnService.userID;
-     console.log("USERID =" + this.user_ID);
-     this.db.object('Chemistry/users/' + this.user_ID).valueChanges().subscribe(user => {
-       if( user != null )
-       {
-           this.userObject = this.db.object('Chemistry/users/' + this.user_ID);
-           var actualUser : any = Object.values(user);
-           //console.log("THIS-> " + actualUser);
-          
-           this.user_courses = actualUser[1].split(" ");
-           if (this.user_courses[0]==="")this.user_courses = [];
-           console.log("loodydoo",this.user_courses[0]);
-           this.coursesObservable.subscribe(allCourse => {
-             console.log(allCourse[1]);
-             this.courseArray = allCourse;
-             console.log(this.courseArray);
-             let w = 0;
-             let g = 0;
-             for(w=0;w<this.user_courses.length;w++)
-             {
-               for(g=0;g<allCourse.length;g++)
-               {
-                 if(this.user_courses[w] === allCourse[g].courseCode)
-                 {
-                  console.log(allCourse[g]);
-                  allCourse.splice(g,1);
-                  //console.log(allCourse);
-                   
-                 }
-                 //console.log(this.user_courses[w], allCourse[g]);
-               }
-             }
-             this.courseArray = allCourse;
-             console.log(this.courseArray);
-           //  console.log("below");
-           //  console.log(allCourse);
-             
-           })
-           
-           //console.log("Array Holding courses: " + this.user_courses); //USER COURSE CODES
-           //console.log("Array Length: " + this.user_courses.length);
-           //console.log("Course1: " + this.user_courses[0]);
-           //console.log("Course2: " + this.user_courses[1]);
-           var actualCourse: any; 
-           let x = 0;
-           let y=0;
-           for(x=0;x<this.user_courses.length;x++)
-           {
-            //console.log(x);
-            
-             this.db.object('Chemistry/courses/' + this.user_courses[x]).valueChanges().subscribe(course => {
-             if(course !=null){
-                this.listCourses.push(course);
-                console.log("lc below");
-                console.log(this.user_courses[x]);
-                //console.log(this.listCourses);
-                //console.log(x);
-                actualCourse = Object.values(course);
-                //console.log(actualCourse);
-                //console.log("LeeroyJenkins: " + actualCourse);
-                this.user_courseNames[y] = actualCourse[2];
-                y++;
-               // console.log("This user's courses: " + this.user_courseNames);//USER COURSE NAMES
-                
-                
-             }
-             //console.log()
-             })
 
-             
-             /*
-             this.coursesObject = this.db.object('Chemistry/courses/' + this.user_courses[x]);
-             var actualCourse: any = Object.values(this.coursesObject);
-             console.log("PLS =" + actualCourse);
-             */
-             //this.user_courseNames[x] = actualCourse[3];
-           }
-           //console.log(this.user_courseNames);
-           //console.log(this.user_courseNames);
-           //this.user_courses = this.userObject.name;
-           
-           
-           
-       }
-     })
+    this.userList = this.getCourses('/Chemistry/users');
+    this.user_ID = this.lgnService.userID;
+    this.db.object('Chemistry/users/' + this.user_ID).valueChanges().subscribe(user => {
+      if (user != null) {
+        this.userObject = this.db.object('Chemistry/users/' + this.user_ID);
+        var actualUser: any = Object.values(user);
+        this.user_courses = actualUser[1].split(" ");
+        if (this.user_courses[0] === "") this.user_courses = [];
+        this.coursesObservable.subscribe(allCourse => {
+          this.courseArray = allCourse;
+          let w = 0;
+          let g = 0;
+          for (w = 0; w < this.user_courses.length; w++) {
+            for (g = 0; g < allCourse.length; g++) {
+              if (this.user_courses[w] === allCourse[g].courseCode) {
+                allCourse.splice(g, 1);
+
+              }
+            }
+          }
+          this.courseArray = allCourse;
+        })
+
+        var actualCourse: any;
+        let x = 0;
+        let y = 0;
+        for (x = 0; x < this.user_courses.length; x++) {
+          this.db.object('Chemistry/courses/' + this.user_courses[x]).valueChanges().subscribe(course => {
+            if (course != null) {
+              this.listCourses.push(course);
+              actualCourse = Object.values(course);
+              this.user_courseNames[y] = actualCourse[2];
+              y++;
+            }
+          })
+        }
+
+      }
+    })
 
      
 
@@ -154,16 +106,11 @@ export class CoursesComponent implements OnInit {
         var newCourseList: string;
         var courseCompare: string;
         let flag = 1;
-      //  console.log(course);
         var currCourse: any[] = Object.values(course); 
-       // console.log(currCourse);
-        //console.log(currCourse[0]);
-
         this.sub = this.db.object('Chemistry/users/' + this.user_ID).valueChanges().subscribe(user => {
           if(user!=null)
           {
              var currUser: any = Object.values(user);
-            // console.log(currUser);
              courseCompare = currUser[1].split(" ");
              let x = 0;
              for(x=0;x<courseCompare.length;x++)
@@ -181,8 +128,6 @@ export class CoursesComponent implements OnInit {
                 else {
                   newCourseList= currUser[1] + " " + currCourse[0];
                 }
-               // console.log(newCourseList);
-              //  console.log(newCourseList.length);
                 this.db.object('Chemistry/users/' + this.user_ID).update({courseList: newCourseList}).then(_=> {
                   this.listCourses = [];
                 })
@@ -192,19 +137,6 @@ export class CoursesComponent implements OnInit {
           }
         })
         this.isSelected = false;
-        //this.db.object('Chemistry/users/' + this.user_ID).update({courseList: newCourseList});
-
-
-
-
-        //this.db.object('Chemistry/users/' + this.user_ID).update({courseList: weeny[0]});  YEEET
-        
-        //console.log(course);
-       // var useCourse : any = Object.values(course);
-       // console.log("Trying to add this: " + course.courseCode);
-        //this.db.object('Chemistry/users/' + this.user_ID).update({courseList: useCourse.courseCode});
-        //this.isSelected = false;
-        
   }
 
   open(content) {
